53ac89fed79200ec290de446dae41d39fb7e5405
Davi Piassi Barros dos Santos <davibarros-ieg@instituto.ad>
2025-03-27 11:16:01 -0300

    changes

--- data/database/model/character.js
+++ data/database/model/character.js
@@ -3,6 +3,6 @@ import mongoose from 'mongoose';
 const characterSchema = new mongoose.Schema({
     name: String,
     image: String,
-});
+}, { _id: false });
 
 export default characterSchema;
--- data/database/model/ingredient.js
+++ data/database/model/ingredient.js
@@ -3,6 +3,6 @@ import mongoose from 'mongoose';
 const ingredientSchema = new mongoose.Schema({
     name: String,
     amount: Number,
-});
+}, { _id: false });
 
 export default ingredientSchema;
